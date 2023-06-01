b8481d2695a4b838c15d220a83141a84114fc88c
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-06-01 16:46:35 +0200

    problem import main content

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -9,11 +9,23 @@ export default {
 
 <template>
 
-   
+    <div class="card-comic">
+        <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+        <h4>BATMAN</h4>
+    </div>
 
 </template>
 
 <style scoped lang="scss">
 
-    
+.card-comic{
+    width: calc((100% / 6) - 50px);
+
+    h4{
+    color: white;
+    text-align: left;
+    padding: 15px 0;
+    }
+                
+}
 </style>
